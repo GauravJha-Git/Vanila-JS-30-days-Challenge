1eb28af10524a17c1f0616dbd312e275e6599968
GauravJha-Git <168263515+GauravJha-Git@users.noreply.github.com>
2025-03-14 23:57:26 +0530

    first project done

--- 1/1.js
+++ 1/1.js
@@ -1,2 +1,23 @@
-const keys = getElementByID("keys");
-const dataKey = getElementByQuerrySelector("data-key");
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+  audio.play().catch(err => alert("Audio play blocked:", err)); //checking if theres any error after pressing key
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+
+
+  
